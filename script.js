b9cc68e995b8c618c4645865cde63369a5eb3b3e
unknown <ab1748@gmail.com>
2018-09-15 09:26:20 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,154 @@
+function displayCanvas1(){
+    var canvasHTML = document.getElementById('myCanvas1');
+    var contextHTML = canvasHTML.getContext('2d');
+    contextHTML.strokeRect(0,0,canvasHTML.width, canvasHTML.height);
+
+    var d = new Date();                //Получаем экземпляр даты
+    var t_sec = 6*d.getSeconds();                           //Определяем угол для секунд
+    var t_min = 6*(d.getMinutes() + (1/60)*d.getSeconds()); //Определяем угол для минут
+    var t_hour = 30*(d.getHours() - 1 + (1/60)*d.getMinutes()); //Определяем угол для часов
+
+    drawclock(canvasHTML, contextHTML, t_sec, t_min, t_hour);
+    return;
+}
+
+function displayCanvas2(){
+    var canvasHTML = document.getElementById('myCanvas2');
+    var contextHTML = canvasHTML.getContext('2d');
+    contextHTML.strokeRect(0,0,canvasHTML.width, canvasHTML.height);
+
+    var d = new Date();                //Получаем экземпляр даты
+    var t_sec = 6*d.getSeconds();                           //Определяем угол для секунд
+    var t_min = 6*(d.getMinutes() + (1/60)*d.getSeconds()); //Определяем угол для минут
+    var t_hour = 30*(d.getHours() + (1/60)*d.getMinutes()); //Определяем угол для часов
+
+    drawclock(canvasHTML, contextHTML, t_sec, t_min, t_hour);
+    return;
+}
+
+function displayCanvas3(){
+    var canvasHTML = document.getElementById('myCanvas3');
+    var contextHTML = canvasHTML.getContext('2d');
+    contextHTML.strokeRect(0,0,canvasHTML.width, canvasHTML.height);
+
+    var d = new Date();                //Получаем экземпляр даты
+    var t_sec = 6*d.getSeconds();                           //Определяем угол для секунд
+    var t_min = 6*(d.getMinutes() + (1/60)*d.getSeconds()); //Определяем угол для минут
+    var t_hour = 30*(d.getHours()-2 + (1/60)*d.getMinutes()); //Определяем угол для часов
+    drawclock(canvasHTML, contextHTML, t_sec, t_min, t_hour);
+    return;
+}
+
+//прорисовка часов
+function drawclock(canvasHTML, contextHTML, t_sec, t_min, t_hour){
+    //Расчет координат центра и радиуса часов
+    var radiusClock = canvasHTML.width/2 - 10;
+    var xCenterClock = canvasHTML.width/2;
+    var yCenterClock = canvasHTML.height/2;
+
+    //Очистка экрана.
+    contextHTML.fillStyle = "#ffffff";
+    contextHTML.fillRect(0,0,canvasHTML.width,canvasHTML.height);
+
+    //Рисуем контур часов
+    contextHTML.strokeStyle =  "#000000";
+    contextHTML.lineWidth = 1;
+    contextHTML.beginPath();
+    contextHTML.arc(xCenterClock, yCenterClock, radiusClock, 0, 2*Math.PI, true);
+    contextHTML.moveTo(xCenterClock, yCenterClock);
+    contextHTML.stroke();
+    contextHTML.closePath();
+
+    //Рисуем рисочки часов
+    var radiusNum = radiusClock - 10; //Радиус расположения рисочек
+    var radiusPoint;
+    for(var tm = 0; tm < 60; tm++){
+        contextHTML.beginPath();
+        if(tm % 5 == 0){radiusPoint = 5;}else{radiusPoint = 2;} //для выделения часовых рисочек
+        var xPointM = xCenterClock + radiusNum * Math.cos(-6*tm*(Math.PI/180) + Math.PI/2);
+        var yPointM = yCenterClock - radiusNum * Math.sin(-6*tm*(Math.PI/180) + Math.PI/2);
+        contextHTML.arc(xPointM, yPointM, radiusPoint, 0, 2*Math.PI, true);
+        contextHTML.stroke();
+        contextHTML.closePath();
+    }
+
+    //Оцифровка циферблата часов
+    for(var th = 1; th <= 12; th++){
+        contextHTML.beginPath();
+        contextHTML.font = '25px sans-serif';
+        var xText = xCenterClock + (radiusNum - 30) * Math.cos(-30*th*(Math.PI/180) + Math.PI/2);
+        var yText = yCenterClock - (radiusNum - 30) * Math.sin(-30*th*(Math.PI/180) + Math.PI/2);
+        if(th <= 9){
+            contextHTML.strokeText(th, xText - 5 , yText + 10);
+        }else{
+            contextHTML.strokeText(th, xText - 15 , yText + 10);
+        }
+        contextHTML.stroke();
+        contextHTML.closePath();
+
+
+        //Рисуем секунды
+        contextHTML.beginPath();
+        contextHTML.strokeStyle =  "#FF0000";
+        contextHTML.moveTo(xCenterClock, yCenterClock);
+        contextHTML.lineTo(xCenterClock + lengthSeconds*Math.cos(Math.PI/2 - t_sec*(Math.PI/180)),
+            yCenterClock - lengthSeconds*Math.sin(Math.PI/2 - t_sec*(Math.PI/180)));
+        contextHTML.stroke();
+        contextHTML.closePath();
+
+        //Рисуем минуты
+        contextHTML.beginPath();
+        contextHTML.strokeStyle =  "#000000";
+        contextHTML.lineWidth = 3;
+        contextHTML.moveTo(xCenterClock, yCenterClock);
+        contextHTML.lineTo(xCenterClock + lengthMinutes*Math.cos(Math.PI/2 - t_min*(Math.PI/180)),
+            yCenterClock - lengthMinutes*Math.sin(Math.PI/2 - t_min*(Math.PI/180)));
+        contextHTML.stroke();
+        contextHTML.closePath();
+
+        //Рисуем часы
+        contextHTML.beginPath();
+        contextHTML.lineWidth = 5;
+        contextHTML.moveTo(xCenterClock, yCenterClock);
+        contextHTML.lineTo(xCenterClock + lengthHour*Math.cos(Math.PI/2 - t_hour*(Math.PI/180)),
+            yCenterClock - lengthHour*Math.sin(Math.PI/2 - t_hour*(Math.PI/180)));
+        contextHTML.stroke();
+        contextHTML.closePath();
+
+        //Рисуем центр часов
+        contextHTML.beginPath();
+        // contextHTML.strokeStyle =  "#000000";
+        // contextHTML.fillStyle = "#ffffff";
+        contextHTML.lineWidth = 3;
+        contextHTML.arc(xCenterClock, yCenterClock, 5, 0, 2*Math.PI, true);
+        contextHTML.stroke();
+        contextHTML.fill();
+        contextHTML.closePath();
+
+        //Рисуем стрелки
+        var lengthSeconds = radiusNum - 10;
+        var lengthMinutes = radiusNum - 15;
+        var lengthHour = lengthMinutes / 1.5;
+
+}
+
+
+}
+window.onload = function(){
+    window.setInterval(
+	function(){
+		var d = new Date();
+        var options1 = { timeZone: 'Europe/Madrid'};
+        var options2 = { timeZone: 'Europe/Kiev'};
+        var options3 = { timeZone: 'Europe/London'};
+		document.getElementById("clock1").innerHTML = d.toLocaleTimeString('en-us', options1);
+        document.getElementById("clock2").innerHTML = d.toLocaleTimeString('en-us', options2);
+        document.getElementById("clock3").innerHTML = d.toLocaleTimeString('en-us', options3);
+		displayCanvas1();
+        displayCanvas2();
+        displayCanvas3();
+	}
+    , 1000);
+}
+
+// define variables
